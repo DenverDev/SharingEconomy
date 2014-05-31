619d65fc0cbd402e1f2cddff87113f99d29cd1e8
irisevenstar <Heather.Stanton@denvergov.org>
2014-05-31 13:30:14 -0600

    Updated with layers

--- SharedEcoUI/public/js/Map.js
+++ SharedEcoUI/public/js/Map.js
@@ -1,22 +1,44 @@
-// JavaScript source code
-define('appMain', [
-    'CCD', "esri/map", "esri/layers/FeatureLayer",
-    "esri/tasks/query", "dijit/TooltipDialog", "dijit/popup",
-    "esri/graphic", "esri/InfoTemplate", "esri/symbols/PictureMarkerSymbol",
-    "esri/renderers/SimpleRenderer", "dijit/registry",
-    "esri/config", "dojo/dom", "dijit/form/CheckBox",
-    "dgrid/OnDemandGrid", "dgrid/Selection", "dojo/_base/declare",
-    "dojo/_base/array", "dojo/store/Memory", "esri/dijit/Geocoder",
-    "dojo/on", "dojo/parser", "dojo/domReady", "dijit/TitlePane"
-], function (
 
-    CCD, Map, FeatureLayer,
-    Query, TooltipDialog, dijitPopup,
-    Graphic, InfoTemplate, PictureMarkerSymbol,
-    SimpleRenderer, registry,
-    esriConfig, dom, CheckBox,
-    OnDemandGrid, Selection, declare,
-    arrayUtils, Memory, Geocoder, on, parser
-){
+var map;
+require(["esri/map", "esri/InfoTemplate", "esri/layers/FeatureLayer", "dojo/domReady!"],
+    function (Map, InfoTemplate, FeatureLayer) {
 
-}
+        map = new Map("map", {
+            basemap: "streets",
+            center: [-104.963767, 39.724628],
+            zoom: 12,
+            slider: true,
+            sliderStyle: "small"
+        });
+        //var pnrRenderer = new SimpleRenderer(new PictureMarkerSymbol('Images/parknride.png', 24, 34));
+        //var rtdLightRailStationRenderer = new SimpleRenderer(new PictureMarkerSymbol('Images/lightrail.png', 24, 34));
+
+        //Set up the pop up for displaying additional information about a point
+        var rtdInfoTemplate = new InfoTemplate({
+            title: "${NAME}",
+            content: "<b>Address:</b> ${ADDRESS}<br/>"
+        });
+
+        var pnrLayer = new FeatureLayer("http://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/SharedTransportation/FeatureServer/0", {
+            id: "pnr",
+            mode: FeatureLayer.MODE_ONDEMAND,
+            infoTemplate: rtdInfoTemplate,
+            outFields: ['NAME', 'ADDRESS']
+        });
+        //pnrLayer.renderer = pnrRenderer;
+
+        var lightRailStationLayer = new FeatureLayer("http://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/SharedTransportation/FeatureServer/1", {
+            id: "lightrailstations",
+            mode: FeatureLayer.MODE_ONDEMAND,
+            infoTemplate: rtdInfoTemplate,
+            outFields: ['NAME', 'ADDRESS']
+        });
+        //lightRailStationLayer.renderer - rtdLightRailStationRenderer;
+
+        var lightRailLayer = new FeatureLayer("http://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/SharedTransportation/FeatureServer/4", {
+            id: "lightraillines",
+            mode: FeatureLayer.MODE_ONDEMAND
+        });
+
+        map.addLayers([pnrLayer, lightRailStationLayer, lightRailLayer]);
+    })
