eecf7aeb27ec8e8b99fed25059361cbc4c5c7adb
irisevenstar <Heather.Stanton@denvergov.org>
2014-05-31 08:49:34 -0600

    The start of a map

---
+++ Map.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+define('appMain', [
+    'CCD', "esri/map", "esri/layers/FeatureLayer",
+    "esri/tasks/query", "dijit/TooltipDialog", "dijit/popup",
+    "esri/graphic", "esri/InfoTemplate", "esri/symbols/PictureMarkerSymbol",
+    "esri/renderers/SimpleRenderer", "dijit/registry",
+    "esri/config", "dojo/dom", "dijit/form/CheckBox",
+    "dgrid/OnDemandGrid", "dgrid/Selection", "dojo/_base/declare",
+    "dojo/_base/array", "dojo/store/Memory", "esri/dijit/Geocoder",
+    "dojo/on", "dojo/parser", "dojo/domReady", "dijit/TitlePane"
+], function (
+
+    CCD, Map, FeatureLayer,
+    Query, TooltipDialog, dijitPopup,
+    Graphic, InfoTemplate, PictureMarkerSymbol,
+    SimpleRenderer, registry,
+    esriConfig, dom, CheckBox,
+    OnDemandGrid, Selection, declare,
+    arrayUtils, Memory, Geocoder, on, parser
+){
+
+}
