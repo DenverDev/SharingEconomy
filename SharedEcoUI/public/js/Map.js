3e4ad5db29d21ff4ee4c79534e25f25275f906c8
David Strong <dstrong@locationinsight.com>
2014-06-01 00:41:40 -0600

    flyouts

--- SharedEcoUI/public/js/Map.js
+++ SharedEcoUI/public/js/Map.js
@@ -24,7 +24,8 @@ require(["esri/map", "esri/InfoTemplate", "esri/layers/FeatureLayer", "esri/laye
 		bcycleTemplate = $("#bcycle_view");
 		bcycleTemplate = _.template( bcycleTemplate.html() );
 		var bcycleInfoTemplate = new InfoTemplate();
-		bcycleData = function(graphic) {
+		bcycleInfoTemplate.setTitle('B-Cycle ${STATION_NA}');
+		bcycleInfoTemplate.setContent(function(graphic) {
 			bcycleObj = {
 				'street' : graphic.attributes.ADDRESS_LI,
 				'city' : graphic.attributes.CITY,
@@ -33,16 +34,24 @@ require(["esri/map", "esri/InfoTemplate", "esri/layers/FeatureLayer", "esri/laye
 				'docs' : graphic.attributes.NUM_DOCKS
 			};
 			return bcycleTemplate(bcycleObj);
-		};
-		bcycleInfoTemplate.setTitle('B-Cycle ${STATION_NA}');
-		bcycleInfoTemplate.setContent(bcycleData);
+		});
 
-		pnrTemplate = $('#pnr_view');
-		pnrTemplate = _.template( pnrTemplate.html() );
-		var pnrInfoTemplate = new InfoTemplate();
-		pnrData = function(graphic) {
+		rtdTemplate = $('#pnr_view');
+		rtdTemplate = _.template( rtdTemplate.html() );
+		var rtdInfoTemplate = new InfoTemplate();
+		rtdInfoTemplate.setTitle(function(graphic){
+			if (graphic.attributes.CLASS) {
+				var locName = graphic.attributes.CLASS
+			} else if (graphic.attributes.STOPNAME) {
+				var locName = 'Stop'
+			} else {
+				var locName = 'LRT'
+			}
+			return locName + ' ' + (graphic.attributes.NAME ? graphic.attributes.NAME : graphic.attributes.STOPNAME);
+		});
+		rtdInfoTemplate.setContent(function(graphic) {
 			pnrObj = {
-				'street' : graphic.attributes.ADDRESS,
+				'street' : (graphic.attributes.ADDRESS ? graphic.attributes.ADDRESS : graphic.attributes.STOPNAME),
 				'city' : graphic.attributes.CITY,
 				'state' : 'CO',
 				'zip' : graphic.attributes.ZIPCODE,
@@ -52,22 +61,16 @@ require(["esri/map", "esri/InfoTemplate", "esri/layers/FeatureLayer", "esri/laye
 					'limited' : graphic.attributes.LIMITED_RT,
 					'regional' : graphic.attributes.REGIONAL_R,
 					'skyride' : graphic.attributes.SKYRIDE_RT,
-					'lightrail' : graphic.attributes.LINE
+					'lightrail' : graphic.attributes.LINE,
+					'routes' : graphic.attributes.ROUTES
 				},
 				'parking' : graphic.attributes.AUTOS,
 				'racks' : graphic.attributes.RACKS,
 				'lockers' : graphic.attributes.LOCKERS,
 				'shelters' : graphic.attributes.SHELTERS
 			};
-			return pnrTemplate(pnrObj);
-		};
-		pnrInfoTemplate.setTitle('PNR ${NAME}');
-		pnrInfoTemplate.setContent(pnrData);
-
-        var rtdInfoTemplate = new InfoTemplate({
-            title: "${NAME}",
-            content: "<b>Address:</b> ${ADDRESS}<br/>"
-        });
+			return rtdTemplate(pnrObj);
+		});
 
         //Set up the tooltip for hovering over points
         var dialog = new TooltipDialog({
@@ -105,18 +108,17 @@ require(["esri/map", "esri/InfoTemplate", "esri/layers/FeatureLayer", "esri/laye
         pnrLayer = new FeatureLayer("http://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/BruceSharedTransportation/FeatureServer/1", {
             id: "pnr",
             mode: FeatureLayer.MODE_ONDEMAND,
-            infoTemplate: pnrInfoTemplate,
-            outFields: ['NAME', 'ADDRESS', 'CITY', 'ZIPCODE', 'PID', 'CLASS', 'LOCAL_RT', 'EXPRESS_RT', 'LIMITED_RT', 'REGIONAL_R', 'SKYRIDE_RT', 'LINE', 'AUTOS', 'RACKS', 'LOCKERS', 'SHELTERS']
-
-
+            infoTemplate: rtdInfoTemplate,
+            outFields: ['NAME', 'ADDRESS', 'CLASS', 'CITY', 'ZIPCODE', 'LOCAL_RT', 'EXPRESS_RT', 'LIMITED_RT', 'REGIONAL_R', 'SKYRIDE_RT', 'LINE', 'AUTOS', 'RACKS', 'LOCKERS', 'SHELTERS']
         });
+
         pnrLayer.renderer = pnrRenderer;
 
         lightRailStationLayer = new FeatureLayer("http://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/BruceSharedTransportation/FeatureServer/2", {
             id: "lightrailstations",
             mode: FeatureLayer.MODE_ONDEMAND,
             infoTemplate: rtdInfoTemplate,
-            outFields: ['NAME', 'ADDRESS']
+            outFields: ['NAME', 'ADDRESS', 'AUTOS', 'RACKS', 'LOCKERS', 'SHELTERS']
         });
         lightRailStationLayer.renderer = rtdLightRailStationRenderer;
         
@@ -161,7 +163,7 @@ require(["esri/map", "esri/InfoTemplate", "esri/layers/FeatureLayer", "esri/laye
         councilLayer = new FeatureLayer("http://services1.arcgis.com/zdB7qR0BtYrg0Xpl/arcgis/rest/services/BruceSharedTransportation/FeatureServer/6", {
             id: "councildistricts",
             mode: FeatureLayer.MODE_ONDEMAND,
-            outFields: ['DIST_REP', 'DIST_NUM']
+            outFields: ['DIST_REP']
         });
 
         var councilLabelLayer = new LabelLayer();
@@ -171,7 +173,7 @@ require(["esri/map", "esri/InfoTemplate", "esri/layers/FeatureLayer", "esri/laye
         textSymbol.setFont(font);
         textSymbol.setColor(color);
         var councilLabelRenderer = new SimpleRenderer(textSymbol);
-        councilLabelLayer.addFeatureLayer(councilLayer, councilLabelRenderer, "District ${DIST_NUM}: ${DIST_REP}");
+        councilLabelLayer.addFeatureLayer(councilLayer, councilLabelRenderer, "${DIST_REP}");
         councilLabelLayer.minScale = "100000";
         councilLabelLayer.maxScale = "40000"
 
