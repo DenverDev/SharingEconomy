755ff44f04d89bab8f29d74adb676f9fc100f88e
Jesse Porter <jesseporter19@gmail.com>
2014-06-01 05:46:03 -0400

    carshare live
    use new js file public/js/cartogo.js with function getCarToGo('Denver')
    and getLyft('Denver') in app

--- SharedEcoUI/public/js/cartogo.js
+++ SharedEcoUI/public/js/cartogo.js
@@ -54,7 +54,9 @@
                 });
     }
 
-         function getLyft(loc,Scope){  
+         function getLyft(loc,Scope,loop){  
+
+          // Simulation on basis of an API endpoint for 3rd parties and their agents
 
            $jQ = jQuery.noConflict();
            $jQ.ajax(
@@ -80,22 +82,44 @@
                                 togoArray.push(line)
                            }
 
-                        // send to arcGis Map Layer
-                        var cartogoLayer = new esri.layers.GraphicsLayer({
+                         // send to arcGis Map Layer
+                         lyftLayer = map.getLayer('lyft');
+                           if(lyftLayer){ 
+                            lyftLayer.clear(); 
+                           }else{
+                            lyftLayer = new esri.layers.GraphicsLayer({
                                 visible         : true,
                                 id              : 'lyft',
-                        });
- 
-                        map.addLayers([cartogoLayer]);
-                        
+                            });
+                          }
+                                 
+                        //map.addLayers([cartogoLayer]);
                         $jQ.each(togoArray, function(i,cars) {
-                          if(i<50){ 
+                         if(loop===undefined && i<35){
                            var point = new esri.geometry.Point(cars["lon"],cars["lat"]);
                            var symbol = new esri.symbol.PictureMarkerSymbol('./public/Images/lyft.png',25, 36);
                            var graphic = new esri.Graphic(point, symbol);
-                           cartogoLayer.add(graphic);
-                          }
+                           lyftLayer.add(graphic);
+                         }else if(i>10 && i<50 && loop==1){
+                           var point = new esri.geometry.Point(cars["lon"]+0.008,cars["lat"]+0.011);
+                           var symbol = new esri.symbol.PictureMarkerSymbol('./public/Images/lyft.png',25, 36);
+                           var graphic = new esri.Graphic(point, symbol);
+                           lyftLayer.add(graphic);
+                         }else if(i>15 && i<60 && loop==2){
+                           var point = new esri.geometry.Point(cars["lon"]+0.016,cars["lat"]+0.019);
+                           var symbol = new esri.symbol.PictureMarkerSymbol('./public/Images/lyft.png',25, 36);
+                           var graphic = new esri.Graphic(point, symbol);
+                           lyftLayer.add(graphic);
+                         }else if(i>25 && i<65){
+                           var point = new esri.geometry.Point(cars["lon"]+0.024,cars["lat"]+0.029);
+                           var symbol = new esri.symbol.PictureMarkerSymbol('./public/Images/lyft.png',25, 36);
+                           var graphic = new esri.Graphic(point, symbol);
+                           lyftLayer.add(graphic);
+                         }
+
                         });
+
+                     map.addLayers([lyftLayer]);
 
                     },
                     error: function (msg, url, line) {
@@ -103,4 +127,15 @@
                         console.log('cartogo input error - msg = ' + msg + ', url = ' + url + ', line = ' + line);
                     }
                 });
+        
+        delete lyftLayer;
+          var numOfCalls = 0;
+         if(loop===undefined){ 
+          var intervalID = setInterval(function(){ 
+            getLyft(loc,'',numOfCalls); 
+            numOfCalls++;
+              if(numOfCalls == 5){ window.clearInterval(intervalID); }
+          },2000);
+         }
+  
     }
