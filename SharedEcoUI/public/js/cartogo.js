fc0e39c3cb358924a6b03447d8f11e76a246272b
Jesse Porter <jesseporter19@gmail.com>
2014-05-31 21:05:09 -0400

    add Rideshare(CarToGo) to map
    use javascript function getCarToGo('Denver') to add real-time locations
    of available cars to map

---
+++ SharedEcoUI/public/js/cartogo.js
@@ -0,0 +1,55 @@
+     function getCarToGo(loc,Scope){  
+
+           $jQ = jQuery.noConflict();
+           $jQ.ajax(
+                {
+                    type: "GET",
+                    url: ('https://www.car2go.com/api/v2.1/parkingspots'),
+                    data: {
+                    	 loc:loc,
+                         oauth_consumer_key: 'car2gowebsite',
+                         format: 'json'
+                         },
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "jsonp",
+                    success: function (data) {
+                        var outData = '';
+                        var togoArray = new Array();
+
+                        //parse data into array
+                           for (i = 0; i < Object.keys(data.placemarks).length; ++i) { 
+                                var line = new Array();
+                                line["lon"]    = data.placemarks[i].coordinates[0];
+                                line["lat"]    = data.placemarks[i].coordinates[1];
+                                line["pulgIn"] = data.placemarks[i].chargingPole;
+                                line["desc"]   = data.placemarks[i].name;
+                                line["active"] = data.placemarks[i].usedCapacity;   
+                                togoArray.push(line)
+                           }
+
+                        // send to arcGis Map Layer
+                        var cartogoLayer = new esri.layers.GraphicsLayer({
+                                visible         : true,
+                                id              : 'cartogo',
+                        });
+ 
+                        map.addLayers([cartogoLayer]);
+                        
+                        $jQ.each(togoArray, function(i,cars) {
+                           var point = new esri.geometry.Point(cars["lon"],cars["lat"]);
+                           if(cars["active"]==1){ 
+                           var symbol = new esri.symbol.PictureMarkerSymbol('./public/Images/carshare_inactive.png',25, 36);
+                           }else{
+                           var symbol = new esri.symbol.PictureMarkerSymbol('./public/Images/carshare.png',25, 36);
+                           }
+                           var graphic = new esri.Graphic(point, symbol);
+                           cartogoLayer.add(graphic);
+                        });
+                                                                                  $jQ('#testOutput').html(outData);
+                    },
+                    error: function (msg, url, line) {
+                        console.log('cartogo input error - error trapped in error: function(msg, url, line)');
+                        console.log('cartogo input error - msg = ' + msg + ', url = ' + url + ', line = ' + line);
+                    }
+                });
+    }
